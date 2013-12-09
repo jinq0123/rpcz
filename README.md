c1a5b33298cb9124aba318728f524f8107e119b5
Jin Qing <jinq0123@163.com>
2013-12-09 20:24:50 +0800

    build example in VC

--- README.md
+++ README.md
@@ -13,9 +13,15 @@ If Windows have no boost dynamic libraries,
     CMake Error at D:/Program Files/CMake 2.8/share/cmake-2.8/Modules/FindBoost.cmake:1192 (message):
       Unable to find the requested Boost libraries.
 
-then "Add Entry" on cmake-gui: Boost_USE_STATIC_LIBS, 
-Or add line in CMakeLists.txt: SET(Boost_USE_STATIC_LIBS TRUE) 
+then add entry Boost_USE_STATIC_LIBS on cmake-gui,
+or add a line in CMakeLists.txt:
+
+    SET(Boost_USE_STATIC_LIBS TRUE) 
 
 VC build need to check on rpcz_build_static, 
 because rpcz.dll will not produce lib file,
 which zsendrpc need.
+
+Only support msvc Release version. 
+(Using release version of libprotobuf.lib,
+ and need plugin/cpp/Release/protoc-gen-cpp_rpcz.exe.)
--- cmake_modules/rpcz_functions.cmake
+++ cmake_modules/rpcz_functions.cmake
@@ -1,7 +1,13 @@
-set(RPCZ_PLUGIN_ROOT ${CMAKE_CURRENT_LIST_DIR}/../build/src/rpcz/plugin)
+set(RPCZ_PLUGIN_ROOT ${CMAKE_BINARY_DIR}/src/rpcz/plugin)
 
 function(PROTOBUF_GENERATE_RPCZ SRCS HDRS)
-  set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/cpp/protoc-gen-cpp_rpcz)
+  if(MSVC)
+    # Only support msvc Release version.
+    set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/cpp/Release/protoc-gen-cpp_rpcz.exe)
+  else(MSVC)
+    set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/cpp/protoc-gen-cpp_rpcz)
+  endif(MSVC)
+  
   PROTOBUF_GENERATE_MULTI(PLUGIN "cpp_rpcz" PROTOS ${ARGN}
                           OUTPUT_STRUCT "_SRCS:.rpcz.cc;_HDRS:.rpcz.h"
                           FLAGS "--plugin=protoc-gen-cpp_rpcz=${PLUGIN_BIN}"
@@ -11,7 +17,13 @@ function(PROTOBUF_GENERATE_RPCZ SRCS HDRS)
 endfunction()
 
 function(PROTOBUF_GENERATE_PYTHON_RPCZ SRCS)
-  set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/python/protoc-gen-python_rpcz)
+  if(MSVC)
+    # Only support msvc Release version.
+    set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/python/Release/protoc-gen-python_rpcz.exe)
+  else(MSVC)
+    set(PLUGIN_BIN ${RPCZ_PLUGIN_ROOT}/python/protoc-gen-python_rpcz)
+  endif(MSVC)
+  
   PROTOBUF_GENERATE_MULTI(PLUGIN "python_rpcz" PROTOS ${ARGN}
                           OUTPUT_STRUCT "_SRCS:_rpcz.py"
                           FLAGS "--plugin=protoc-gen-python_rpcz=${PLUGIN_BIN}"
