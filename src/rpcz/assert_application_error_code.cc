d9d289a71bf3199ae9c7181ee965a89a1298bccc
Jin Qing <jinq0123@163.com>
2015-01-28 15:34:21 +0800

    Revert "delete rpc_response_header.application_error_code"
    This reverts commit fddb73c32c5b094ea1b1546af780d2969a24c110.

---
+++ src/rpcz/assert_application_error_code.cc
@@ -0,0 +1,18 @@
+// Author: Jin Qing (http://blog.csdn.net/jq0123)
+// Assert applicatioin error code values.
+
+#include <boost/static_assert.hpp>
+#include <rpcz/application_error_code.hpp>
+#include <rpcz/rpcz.pb.h>
+
+namespace rpcz {
+BOOST_STATIC_ASSERT(error_code::INVALID_HEADER == rpc_response_header::INVALID_HEADER);
+BOOST_STATIC_ASSERT(error_code::NO_SUCH_SERVICE == rpc_response_header::NO_SUCH_SERVICE);
+BOOST_STATIC_ASSERT(error_code::NO_SUCH_METHOD == rpc_response_header::NO_SUCH_METHOD);
+BOOST_STATIC_ASSERT(error_code::INVALID_MESSAGE == rpc_response_header::INVALID_MESSAGE);
+BOOST_STATIC_ASSERT(error_code::METHOD_NOT_IMPLEMENTED == rpc_response_header::METHOD_NOT_IMPLEMENTED);
+BOOST_STATIC_ASSERT(error_code::TIMEOUT_EXPIRED == rpc_response_header::TIMEOUT_EXPIRED);
+
+// warning LNK4221: This object file does not define any previously undefined public symbols, so it will not be used by any link operation that consumes this library
+char _ASSERT_APPLICATION_ERROR_CODE_CC_[] = "";
+}  // namespace rpcz
